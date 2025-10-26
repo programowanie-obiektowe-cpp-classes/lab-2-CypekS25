a92912c035d7d2fbe0b55623911274478357c39e
CypekS25 <CypSto@gmail.com>
2025-10-26 18:14:39 +0100

    solved

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,5 +4,50 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+private:
+    // wskaznik
+    Resource* resource_ptr;
+
+public:
+    // konstruktor domyœlny
+    ResourceManager() { resource_ptr = new Resource(); }
+
+    // destruktor
+    ~ResourceManager() { delete resource_ptr; }
+
+    // konstruktor kopiuj¹cy
+    ResourceManager(const ResourceManager& other)
+    {
+        resource_ptr = new Resource(*(other.resource_ptr));
+    }
+
+    // operator przypisania kopiuj¹cy
+    ResourceManager& operator=(const ResourceManager& other)
+    {
+        if (this != &other) {
+            Resource* new_resource = new Resource(*(other.resource_ptr));
+            delete resource_ptr;
+            resource_ptr = new_resource;
+        }
+        return *this;
+    }
+
+    // konstruktor przenosz¹cy
+    ResourceManager(ResourceManager&& other) noexcept // segestia vs
+    {
+        resource_ptr = other.resource_ptr;
+    }
+
+    // operator przypisania przenosz¹cy
+    ResourceManager& operator=(ResourceManager&& other) noexcept // segestia vs
+    {
+        if (this != &other) {
+            delete resource_ptr;
+            resource_ptr = other.resource_ptr;
+        }
+        return *this;
+    }
+
+    // metoda get()
+    double get() const { return resource_ptr->get(); }
 };
